edb34736bea85cb431c0c13ff3b0ae8d61f408bb
hllindblom <hllindblom@gmail.com>
2017-11-15 12:59:16 +0200

    Mainin päivitys käyttämään tekstikäyttistä

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,22 @@ import tietokantaobjektit.Vinkki;
 import dao.VinkkiDAO;
 import dao.Tietokanta;
 import java.sql.SQLException;
+import logiikka.Logiikka;
+import tekstikayttis.*;
 
 public class Main {
     public static void main(String[] args) {
-		// Lisää vinkin tietokantaan. Suorita ja katso esim sqlite3:lla.
-		Vinkki vinkki = new Vinkki("Testiotsikko", "Testikuvaus", "Kirja");
-		Tietokanta db = new Tietokanta("jdbc:sqlite:TietokantaTest1.db");
-		VinkkiDAO dao = new VinkkiDAO(db);
-		dao.lisaaVinkki(vinkki);
+        // Lisää vinkin tietokantaan. Suorita ja katso esim sqlite3:lla.
+	//Vinkki vinkki = new Vinkki("Testiotsikko", "Testikuvaus", "Kirja");
+	//Tietokanta db = new Tietokanta("jdbc:sqlite:TietokantaTest1.db");
+	//VinkkiDAO dao = new VinkkiDAO(db);
+	//dao.lisaaVinkki(vinkki);
+
+	Tietokanta db = new Tietokanta("jdbc:sqlite:TietokantaTest1.db");
+        Logiikka logiikka = new Logiikka(db);
+	IO io = new IO();
+
+	Tekstikayttis kali = new Tekstikayttis(logiikka, io);
+        kali.kayttoliittyma();
     }
 }
